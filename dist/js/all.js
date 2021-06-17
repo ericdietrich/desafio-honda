62200cc22c021452819664bc2332c19fbb07112e
Eric Dietrich <ericstefanon@gmail.com>
2021-06-16 23:49:10 -0300

    adicionado mais seções ao projeto

--- dist/js/all.js
+++ dist/js/all.js
@@ -1,3 +1,5 @@
 console.log("et"),console.log("object");
-console.log("te");
+
+
+function createSlick(){$(".accessories-slick").slick({dots:!0,infinite:!1,speed:300,slidesToShow:4,slidesToScroll:4,responsive:[{breakpoint:1024,settings:{slidesToShow:3,slidesToScroll:3,infinite:!0,dots:!0}},{breakpoint:600,settings:{slidesToShow:2,slidesToScroll:2}},{breakpoint:480,settings:{slidesToShow:1,slidesToScroll:1}}]})}$(document).on("ready",createSlick);
 //# sourceMappingURL=all.js.map
